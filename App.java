b9108ac3a9d30367fe73c435cf5bdc44a601548f
Zarpperia <castiellgames@gmail.com>
2024-04-29 21:45:43 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        System.out.println("O fatorial de "+numero+" é igual a ..."+fatorial(numero));
+        System.out.println("O fatorial de "+numero+" é igual a ..."+fatorialRecursivo(numero));
+    }
+    //Versão sem recursividade . . .
+    public static int fatorial(int n) {
+        int resultado = 1;
+        for (int i = n; i >= 1; i--) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+
+    //Versão com recursividade . . .
+    public static int fatorialRecursivo(int n ) {
+        if (n < 0) return -1;
+        if(n < 2) return 1;
+
+       return fatorialRecursivo(n-1);
+
+        }
+    }
